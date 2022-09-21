9a1da39dd52b79e95a8fc00bd51b2d6c2e6623a1
IlyaD <kapamepuka@gmail.com>
2022-09-21 21:28:43 +0300

    Задача 15

--- Program.cs
+++ Program.cs
@@ -222,18 +222,34 @@
 // 32679 -> 6
 
 
-Console.WriteLine("Введите число ");
+// Console.WriteLine("Введите число ");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// string xa = Convert.ToString(x);
+
+// if (xa.Length > 2)
+// {
+//   Console.WriteLine("третья цифра -> " + xa[2]);
+// }
+// else 
+// {
+//   Console.WriteLine("-> третьей цифры нет");
+// }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число от 1 до 7");
 int x = Convert.ToInt32(Console.ReadLine());
 
-string xa = Convert.ToString(x);
-
-if (xa.Length > 2)
+if ( x > 5)
 {
-  Console.WriteLine("третья цифра -> " + xa[2]);
+    Console.WriteLine("Выходной");
 }
-else 
+else
 {
-  Console.WriteLine("-> третьей цифры нет");
+    Console.WriteLine("Рабочий");
 }
-
-
