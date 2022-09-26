709723bdcf555bd1634e060aff708e6ffa07be1e
IlyaD <kapamepuka@gmail.com>
2022-09-26 15:04:53 +0300

    задача 19

--- Program.cs
+++ Program.cs
@@ -242,14 +242,88 @@
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Введите число от 1 до 7");
-int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число от 1 до 7");
+// int x = Convert.ToInt32(Console.ReadLine());
 
-if ( x > 5)
+// if ( x > 5)  
+// {
+//     Console.WriteLine("Выходной");
+// }
+// else
+// {
+//     Console.WriteLine("Рабочий");
+// }
+
+
+// Задача 17. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+
+
+
+// Задача 18. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Console.WriteLine("Введите число ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// if (x == 1) Console.WriteLine("x > 0, y > 0");
+// else if (x == 2) Console.WriteLine("x < 0, y > 0");
+// else if (x == 3) Console.WriteLine("x < 0, y < 0");
+// else if (x == 4) Console.WriteLine("x > 0, y < 0");
+// else Console.WriteLine("Такой четверти нет");
+
+
+// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+// Console.WriteLine("Введите координаты х точки А");
+// int a1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты у точки А");
+// int a2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты х точки В");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты у точки В");
+// int b2 = Convert.ToInt32(Console.ReadLine());
+
+// double ab = Math.Sqrt(Math.Pow((a2-a1), 2) + Math.Pow((b2-b1), 2));
+
+// Console.WriteLine($" Расстояние между точками А и В = {ab}");
+
+// Задача 22. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+// Console.WriteLine("Введите число ");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// int i = 0;
+// while (i < x)
+// {
+//     Console.Write($"{Math.Pow(i, 2)} "); i++;
+// }
+
+
+// 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+
+
+string number = Console.ReadLine();
+
+if (number.Length != 5)
 {
-    Console.WriteLine("Выходной");
+    Console.WriteLine("Введите правильное число");
 }
-else
+    if (number[0] == number[4] & number[1] == number[3])
 {
-    Console.WriteLine("Рабочий");
+    Console.WriteLine($"Ваше число {number} палиндроп.");
 }
+    else Console.WriteLine($"Ваше число {number} - НЕ палиндром.");
+
+
