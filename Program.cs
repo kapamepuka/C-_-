ac39a336ad05fe3c54a0d3969717203f06b21a64
IlyaD <kapamepuka@gmail.com>
2022-10-02 14:10:40 +0300

    задача 29

--- Program.cs
+++ Program.cs
@@ -586,17 +586,17 @@
 
 // 9012 -> 12
 
-Console.Clear();
-Console.WriteLine("Введите число");
-int n = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-while (n > 0)
-{
-    int b = n % 10;
-    n = n / 10;
-    sum = sum + b;
-}
-Console.WriteLine(sum);
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// while (n > 0)
+// {
+//     int b = n % 10;
+//     n = n / 10;
+//     sum = sum + b;
+// }
+// Console.WriteLine(sum);
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
@@ -604,11 +604,21 @@ Console.WriteLine(sum);
 
 // 6, 1, 33 -> [6, 1, 33]
 
+int [] array = new int[8];
+int i  = 0;
+while (i < array.Length)
+{
+    array[i] = new Random().Next(0,10);
+    i++;
+}
+Console.WriteLine(String.Join(" ",array));
 
 
 // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
